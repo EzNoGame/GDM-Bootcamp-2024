ef13f726dd7084a8964828c2928c00e2b384a745
EzNoGame <ez041126@gmail.com>
2025-01-09 18:21:18 -0500

    Update CameraControl.cs

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -8,7 +8,7 @@ public class CameraController : MonoBehaviour
  public GameObject player;
 
  // The distance between the camera and the player.
- private Vector3 offset;
+ private Vector3 _offset;
 
  // Start is called before the first frame update.
  void Start()
